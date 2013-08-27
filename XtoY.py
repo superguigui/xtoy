1001e5651b518a1b00e3ca177f3f8a42e909c274
Damien Mortini <ddotmnn+git@gmail.com>
2013-08-27 15:34:30 +0100

    add missing pattern (height->width) and make PATTERNS global

--- XtoY.py
+++ XtoY.py
@@ -1,6 +1,28 @@
 import sublime, sublime_plugin, re
 
 class XtoyCommand(sublime_plugin.TextCommand):
+	global PATTERNS
+	PATTERNS = {
+		"WIDTH"  	:"HEIGHT",
+		"HEIGHT" 	:"WIDTH",
+		"X"      	:"Y",
+		"Y"      	:"X",
+		"width"  	:"height",
+		"height"  	:"width",
+		"Width"  	:"Height",
+		"Height" 	:"Width",
+		"x"      	:"y",
+		"y"      	:"x",
+		"-left"		:"-right",
+		"-right"	:"-left",
+		"-top"		:"-bottom",
+		"-bottom"	:"-top",
+		"left"		:"right",
+		"right"		:"left",
+		"top"		:"bottom",
+		"bottom"	:"top"
+	}
+
 	def run(self, edit):
 		for region in self.view.sel():
 			if region.empty():
@@ -11,33 +33,13 @@ class XtoyCommand(sublime_plugin.TextCommand):
 				self.view.insert(edit, region.end(), self.replace(self.view.substr(region)))
 
 	def replace(self, content):
-		patterns = {
-			"WIDTH"  	:"HEIGHT",
-			"HEIGHT" 	:"WIDTH",
-			"X"      	:"Y",
-			"Y"      	:"X",
-			"width"  	:"height",
-			"Width"  	:"Height",
-			"Height" 	:"Width",
-			"x"      	:"y",
-			"y"      	:"x",
-			"-left"		:"-right",
-			"-right"	:"-left",
-			"-top"		:"-bottom",
-			"-bottom"	:"-top",
-			"left"		:"right",
-			"right"		:"left",
-			"top"		:"bottom",
-			"bottom"	:"top"
-		}
-
 		ignoredSyntaxRegex = re.compile("(.*)[\d][p][x](.*)")
 
 		# I don't know why it's not working with ignoredSyntaxRegex.match(content) each time it returns None...
 		# That's why I'm using findall.
 		if len(ignoredSyntaxRegex.findall(content)) > 0:
-			del patterns['x']
+			del PATTERNS['x']
 
-		regex = re.compile("|".join(map(re.escape, patterns.keys())))
+		regex = re.compile("|".join(map(re.escape, PATTERNS.keys())))
 
-		return regex.sub(lambda match: patterns[match.group(0)], content)
+		return regex.sub(lambda match: PATTERNS[match.group(0)], content)
